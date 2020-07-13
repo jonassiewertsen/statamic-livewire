50b909e8fa174192099531deb1f67142e92a1c09
Jonas Siewertsen <jonas.siewertsen@visuellverstehen.de>
2020-07-13 23:05:17 +0200

    Formatting

--- src/Tags/Livewire.php
+++ src/Tags/Livewire.php
@@ -17,28 +17,29 @@ class Livewire extends \Statamic\Tags\Tags
      * @param $expression
      * @return mixed
      */
-    public function wildcard($expression) {
+    public function wildcard($expression)
+    {
         $lastArg = trim(last(explode(',', $expression)));
 
         if (Str::startsWith($lastArg, 'key(') && Str::endsWith($lastArg, ')')) {
             $cachedKey = Str::replaceFirst('key(', '', Str::replaceLast(')', '', $lastArg));
-            $args = explode(',', $expression);
+            $args      = explode(',', $expression);
             array_pop($args);
             $expression = implode(',', $args);
         } else {
-            $cachedKey = "'".Str::random(7)."'";
+            $cachedKey = "'" . Str::random(7) . "'";
         }
 
-        if (! isset($_instance)) {
+        if (!isset($_instance)) {
             $dom = \Livewire\Livewire::mount($expression)->dom;
         } elseif ($_instance->childHasBeenRendered($cachedKey)) {
-            $componentId = $_instance->getRenderedChildComponentId($cachedKey);
+            $componentId  = $_instance->getRenderedChildComponentId($cachedKey);
             $componentTag = $_instance->getRenderedChildComponentTagName($cachedKey);
-            $dom = \Livewire\Livewire::dummyMount($componentId, $componentTag);
+            $dom          = \Livewire\Livewire::dummyMount($componentId, $componentTag);
             $_instance->preserveRenderedChild($cachedKey);
         } else {
             $response = \Livewire\Livewire::mount($expression);
-            $dom = $response->dom;
+            $dom      = $response->dom;
             $_instance->logRenderedChild($cachedKey, $response->id, \Livewire\Livewire::getRootElementTagName($dom));
         }
         return $dom;
@@ -49,7 +50,8 @@ class Livewire extends \Statamic\Tags\Tags
      *
      * {{ livewire:styles }}
      */
-    public function styles() {
+    public function styles()
+    {
         return \Livewire\Livewire::styles();
     }
 
@@ -58,7 +60,8 @@ class Livewire extends \Statamic\Tags\Tags
      *
      * {{ livewire:scripts }}
      */
-    public function scripts() {
+    public function scripts()
+    {
         return \Livewire\Livewire::scripts();
     }
 }
