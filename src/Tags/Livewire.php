df01327dabba4ce5e4c2ae6c9362133bcbd71fcd
Marco Rieser <marco@rieser.dev>
2023-09-02 11:24:39 +0200

    finish up tags

--- src/Tags/Livewire.php
+++ src/Tags/Livewire.php
@@ -2,7 +2,6 @@
 
 namespace Jonassiewertsen\Livewire\Tags;
 
-use Illuminate\Support\Str;
 use Statamic\Tags\Tags;
 
 class Livewire extends Tags
@@ -11,14 +10,10 @@ class Livewire extends Tags
      * This will load your Livewire component in the Antlers view
      *
      * {{ livewire:your-component-name }}
-     *
-     * This is a very rudimentary implementation.
-     * The reference code in Livewire can be found here:
-     * \Livewire\Mechanisms\RenderComponent::livewire()
      */
     public function wildcard($expression): string
     {
-        return \Livewire\Livewire::mount($expression, $this->params->toArray(), Str::random(7));
+        return \Livewire\Livewire::mount($expression, $this->params->toArray());
     }
 
     /**
@@ -34,7 +29,7 @@ class Livewire extends Tags
         $expression = $this->params->get('property');
         $instanceId = $this->context['__livewire']->getId();
 
-        // todo: this condition is just copied from the original code but not implemented nor tested yet
+        // this condition is just copied from the original code but not implemented nor tested yet
         if ((object)($expression) instanceof \Livewire\WireDirective) {
             $value = $expression->value();
             $modifier = $expression->hasModifier('live') ? '.live' : '';
@@ -47,15 +42,11 @@ class Livewire extends Tags
     /**
      * Accessing the Livewire component.
      *
-     * The method is a small variation from /Livewire/LivewireBladeDirectives
-     * A few small changes had to be made to get the correct output.
-     *
      * {{ livewire:this set="('name', 'Jack')" }}
      */
     public function this(): string
     {
-        //TODO[mr]: check (31.08.23 mr)
-        $instanceId = $this->context['_instance']->id;
+        $instanceId = $this->context['__livewire']->getId();
         $action = $this->params->take(1)->toArray();
         $method = key($action);
         $parameters = reset($action);
