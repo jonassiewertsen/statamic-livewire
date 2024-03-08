f8b2f23f0bafc25e1b3457fcda2b0c8882ce1689
Marco Rieser <marco@rieser.dev>
2024-03-08 08:45:34 +0100

    some fixes

--- src/RestoreSite.php
+++ src/RestoreSite.php
@@ -2,23 +2,29 @@
 
 namespace Jonassiewertsen\Livewire;
 
+use Illuminate\Support\Facades\Date;
 use Livewire\Attributes\Locked;
 use Statamic\Facades\Site;
+use Statamic\Statamic;
 
 trait RestoreSite
 {
     #[Locked]
     public string $siteHandle = "";
 
-    public function hydrateRestoreSite(): void
-    {
-        $site = Site::get($this->siteHandle);
-        setlocale(LC_TIME, $site->locale());
-        app()->setLocale($site->shortLocale());
-    }
-
-    protected function storeSiteHandle(): void
+    public function mountRestoreSite(): void
     {
         $this->siteHandle = Site::current()->handle();
+    }
+
+    public function hydrateRestoreSite(): void
+    {
+        Site::setCurrent($this->siteHandle);
+
+        $site = Site::current();
+
+        setlocale(LC_TIME, $site->locale());
+        app()->setLocale($site->lang());
+        Date::setToStringFormat(Statamic::dateFormat());
     }
 }
